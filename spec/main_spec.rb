780085d3b46d4e00507758b9026da34a833d2fc7
Edie Atha <edieatha@gmail.com>
2020-05-23 00:39:24 +0300

    Added Rspc checks for enumerables.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -72,4 +72,60 @@ describe Enumerable do
       expect([1, 2].my_any?(1)).to eql(true)
     end
   end
+
+    describe '#my_none' do
+    it 'Check my_none' do
+      expect([1, 2].my_none?(1)).to eql(false)
+    end
+  end
+
+    describe '#my_none' do
+    it 'Check my_none' do
+      expect([1, 2].my_none?(0)).to eql(true)
+    end
+  end
+
+     describe '#my_count' do
+    it 'Check my_count' do
+      expect([1, 2].my_count).to eql(2)
+    end
+  end
+
+    describe '#my_count' do
+    it 'Check my_count even' do
+      expect([1, 2].my_count(&:even?)).to eql(1)
+    end
+  end
+
+      describe '#my_count' do
+    it 'Check my_count odd' do
+      expect([1, 2].my_count(&:odd?)).to eql(1)
+    end
+  end
+
+     describe '#my_map' do
+    it 'Check my_map' do
+      expect([1, 2, 7, 4, 5].my_map { |x| x * x }).to eql([1, 4, 49, 16, 25])
+    end
+  end
+
+     describe '#my_map' do
+    it 'Check my_map' do
+      expect([1, 2, 7, 4, 5].my_map {|x| x * 2}).to eql([2, 4, 14, 8, 10])
+    end
+  end
+
+  describe '#my_inject' do
+    it 'Check my_inject' do
+      expect((5..10).my_inject(:+)).to eql(45)
+    end
+  end
+
+   describe '#my_inject' do
+    it 'Check my_inject' do
+      expect((5..10).my_inject { |sum, n| sum + n }).to eql(45)
+    end
+  end
+
+
 end

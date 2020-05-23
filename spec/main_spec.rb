2205d6ede0420d95a98c9d7c5b4a7d45b1dd8bb6
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-23 17:40:42 -0600

    Defined global variables for the test cases using let

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,127 +3,132 @@
 require './main'
 
 describe Enumerable do
+  let (:integers_array) { [11, 2, 3, 56, 1] }
+  let (:string_array) { %w[a b c d e] }
+  let (:true_array) { [1, true, 'hi', []] }
+  let (:arrange_array) { (5..10) }
+
   describe '#my_each' do
     it 'Check each method' do
-      expect([11, 2, 3, 56].my_each { |x| p x }).to eql(4)
+      expect(integers_array.my_each { |x| p x }).to eql(5)
     end
   end
 
   describe '#my_each' do
     it 'Check each method' do
-      expect(%w[a b c d].my_each { |x| p x }).to eql(4)
+      expect(string_array.my_each { |x| p x }).to eql(5)
     end
   end
 
   describe '#my_each' do
     it 'Check each method' do
-      expect([1, true, 'hi', []].my_each { |x| p x }).to eql(4)
+      expect(true_array.my_each { |x| p x }).to eql(4)
     end
   end
 
   describe '#my_each_with_index' do
     it 'Check each method' do
-      expect([11, 2, 3, 56].my_each_with_index do |x, y|
+      expect(integers_array.my_each_with_index do |x, y|
         p "item[#{x}] -> #{y}"
-      end).to eql(4)
+      end).to eql(5)
     end
   end
 
   describe '#my_each_with_index' do
     it 'Check each method' do
-      expect(%w[a b c d].my_each_with_index do |x, y|
+      expect(string_array.my_each_with_index do |x, y|
         p "item[#{x}] -> #{y}"
-      end).to eql(4)
+      end).to eql(5)
     end
   end
 
   describe '#my_select' do
     it 'Check my_select' do
-      expect([11, 2, 3, 56].my_select(&:even?)).to eql([2, 56])
+      expect(integers_array.my_select(&:even?)).to eql([2, 56])
     end
   end
 
   describe '#my_select' do
     it 'Check my_select' do
-      expect(%w[a b c d].my_select { |x| x == 'c' }).to eql(['c'])
+      expect(string_array.my_select { |x| x == 'c' }).to eql(['c'])
     end
   end
 
   describe '#my_all' do
     it 'Check my_all' do
-      expect([1, 2].my_all?(Numeric)).to eql(true)
+      expect(integers_array.my_all?(Numeric)).to eql(true)
     end
   end
 
   describe '#my_all' do
     it 'Check my_all' do
-      expect([1, 2].my_all?(String)).to eql(false)
+      expect(integers_array.my_all?(String)).to eql(false)
     end
   end
 
   describe '#my_any' do
     it 'Check my_any' do
-      expect([nil, true, 99].my_any?(Integer)).to eql(true)
+      expect(integers_array.my_any?(Integer)).to eql(true)
     end
   end
 
   describe '#my_any' do
     it 'Check my_any' do
-      expect([1, 2].my_any?(1)).to eql(true)
+      expect(integers_array.my_any?(1)).to eql(true)
     end
   end
 
   describe '#my_none' do
     it 'Check my_none' do
-      expect([1, 2].my_none?(1)).to eql(false)
+      expect(integers_array.my_none?(1)).to eql(false)
     end
   end
 
   describe '#my_none' do
     it 'Check my_none' do
-      expect([1, 2].my_none?(0)).to eql(true)
+      expect(integers_array.my_none?(0)).to eql(true)
     end
   end
 
   describe '#my_count' do
     it 'Check my_count' do
-      expect([1, 2].my_count).to eql(2)
+      expect(integers_array.my_count).to eql(5)
     end
   end
 
   describe '#my_count' do
     it 'Check my_count even' do
-      expect([1, 2].my_count(&:even?)).to eql(1)
+      expect(integers_array.my_count(&:even?)).to eql(2)
     end
   end
 
   describe '#my_count' do
     it 'Check my_count odd' do
-      expect([1, 2].my_count(&:odd?)).to eql(1)
+      expect(integers_array.my_count(&:odd?)).to eql(3)
     end
   end
 
   describe '#my_map' do
     it 'Check my_map' do
-      expect([1, 2, 7, 4, 5].my_map { |x| x * x }).to eql([1, 4, 49, 16, 25])
+      expect(integers_array.my_map { |x| x * x }).to eql([121, 4, 9, 3136, 1])
     end
   end
 
   describe '#my_map' do
     it 'Check my_map' do
-      expect([1, 2, 7, 4, 5].my_map { |x| x * 2 }).to eql([2, 4, 14, 8, 10])
+      expect(integers_array.my_map { |x| x * 2 }).to eql([22, 4, 6, 112, 2])
     end
   end
 
   describe '#my_inject' do
     it 'Check my_inject' do
-      expect((5..10).my_inject(:+)).to eql(45)
+      expect(arrange_array.my_inject(:+)).to eql(45)
     end
   end
 
   describe '#my_inject' do
     it 'Check my_inject' do
-      expect((5..10).my_inject { |sum, n| sum + n }).to eql(45)
+      expect(arrange_array.my_inject { |sum, n| sum + n }).to eql(45)
     end
   end
 end
